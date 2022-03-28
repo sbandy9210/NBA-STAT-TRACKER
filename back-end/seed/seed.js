ac26fba1dc007e650add8737dff213e5f0029e64
unknown <sbandy9210@gmail.com>
2022-03-28 15:20:10 -0500

    began working on backend

---
+++ back-end/seed/seed.js
@@ -0,0 +1,295 @@
+const  east = [
+    {
+        name: "Celtics",
+        location: "Boston, Massachusetts", 
+        wins: "47",
+        losses: "28",
+        winningPercentage: ".627",
+        rank: "1",
+
+    },
+
+    {
+        name: "Heat",
+        location: "Miami, Florida",
+        wins: "47",
+        losses: "28",
+        winningPercentage: ".627" ,
+        rank: "2"
+
+    },
+
+    {
+        name: "Bucks",
+        location: "Milwaukee, Wisconsin", 
+        wins: "46",
+        losses: "28",
+        winningPercentage: ".622",
+        rank: "3"
+
+    },
+
+    {
+        name: "Sixers",
+        location: "Philadelphia, Pennsylvania ", 
+        wins: "46",
+        losses: "28",
+        winningPercentage: ".622",
+        rank: "4"
+
+    },
+
+    {
+        name: "Bulls",
+        location: "Chicago, Illinois", 
+        wins: "43",
+        losses: "31",
+        winningPercentage: ".581",
+        rank: "5"
+
+    },
+
+    {
+        name: "Raptors",
+        location: "Toronto, Canada", 
+        wins: "42",
+        losses: "32",
+        winningPercentage: ".568",
+        rank: "6"
+    },
+
+    {
+        name: "Cavaliers",
+        location: "Cleveland, Ohio", 
+        wins: "41",
+        losses: "33",
+        winningPercentage: ".554",
+        rank: "7"
+
+    },
+
+    {
+        name: "Hornets",
+        location: "Charlotte, North Carolina", 
+        wins: "39",
+        losses: "36",
+        winningPercentage: ".520",
+        rank: "8"
+
+    }
+]
+
+const west = [
+
+    {
+        name: "Suns",
+        location: "Phoenix, Arizona", 
+        wins: "61",
+        losses: "14",
+        winningPercentage: ".813",
+        rank: "1"
+
+    },
+
+    {
+        name: "Grizzlies",
+        location: "Memphis, Tennessee",
+        wins: "52",
+        losses: "23",
+        winningPercentage: ".693",
+        rank: "2"
+
+    },
+
+    {
+        name: "Warriors",
+        location: "San Francisco, California", 
+        wins: "48",
+        losses: "27",
+        winningPercentage: ".640",
+        rank: "3"
+
+    },
+
+    {
+        name: "Mavericks",
+        location: "Dallas, Texas", 
+        wins: "46",
+        losses: "29",
+        winningPercentage: ".613",
+        rank: "4"
+
+    },
+
+    {
+        name: "Jazz",
+        location: "Salt Lake City, Utah", 
+        wins: "45",
+        losses: "30",
+        percent: ".600",
+        rank: "5"
+
+    },
+
+    {
+        name: "Nuggets",
+        location: "Denver, Colorado", 
+        wins: "44",
+        losses: "31",
+        winningPercentage: ".587",
+        rank: "6"
+
+    },
+
+    {
+        name: "Timberwolves",
+        location: "Minneapolis, Minnesota ", 
+        wins: "43",
+        losses: "33",
+        winningPercentage: ".566",
+        rank: "7"
+
+    },
+
+    {
+        name: "Clippers",
+        location: "Los Angeles, California", 
+        wins: "36",
+        losses: "39",
+        winningPercentage: ".480",
+        rank: "8"
+
+    }
+]
+
+const players = [
+    {
+        team: "Celtics",
+        name: "Jason Tatum",
+        position: "SF",
+        ppg: "27.1",
+        threePoint: "35.6",
+        fieldGoal: "45.0",
+        freeThrow: "86.0",
+        assists: "4.3",
+        blocks: "0.6",
+        steals: "0.9",
+    },
+
+    {
+        team: "Celtics",
+        name: "Jaylen Brown",
+        position: "SG",
+        ppg: "23.4",
+        threePoint: "35.0",
+        fieldGoal: "46.9",
+        freeThrow: "75.8",
+        assists: "3.2",
+        blocks: "0.3",
+        steals: "1.0",
+    },
+
+    {
+        team: "Celtics",
+        name: "Marcus Smart",
+        position: "PG",
+        ppg: "11.9",
+        threePoint: "33.0",
+        fieldGoal: "41.9",
+        freeThrow: "79.9",
+        assists: "5.8",
+        blocks: "0.3",
+        steals: "1.7",
+    },
+
+    {
+        team: "Celtics",
+        name: "Al Horford",
+        position: "C",
+        ppg: "10.0",
+        threePoint: "32.3",
+        fieldGoal: "45.2",
+        freeThrow: "83.7",
+        assists: "3.3",
+        blocks: "1.4",
+        steals: "0.7",
+    },
+
+    {
+        team: "Celtics",
+        name: "Robert Williams",
+        position: "C",
+        ppg: "10.0",
+        threePoint: "0.0",
+        fieldGoal: "73.6",
+        freeThrow: "72.2",
+        assists: "2.0",
+        blocks: "2.2",
+        steals: "0.9",
+    },
+
+    {
+        team: "",
+        name: "",
+        position: "",
+        ppg: "",
+        threePoint: "",
+        fieldGoal: "",
+        freeThrow: "",
+        assists: "",
+        blocks: "",
+        steals: "",
+    },
+
+    {
+        team: "",
+        name: "",
+        position: "",
+        ppg: "",
+        threePoint: "",
+        fieldGoal: "",
+        freeThrow: "",
+        assists: "",
+        blocks: "",
+        steals: "",
+    },
+
+    {
+        team: "",
+        name: "",
+        position: "",
+        ppg: "",
+        threePoint: "",
+        fieldGoal: "",
+        freeThrow: "",
+        assists: "",
+        blocks: "",
+        steals: "",
+    },
+
+    {
+        team: "",
+        name: "",
+        position: "",
+        ppg: "",
+        threePoint: "",
+        fieldGoal: "",
+        freeThrow: "",
+        assists: "",
+        blocks: "",
+        steals: "",
+    },
+
+    {
+        team: "",
+        name: "",
+        position: "",
+        ppg: "",
+        threePoint: "",
+        fieldGoal: "",
+        freeThrow: "",
+        assists: "",
+        blocks: "",
+        steals: "",
+    },
+]
